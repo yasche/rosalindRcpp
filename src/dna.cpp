2a93657d354fd4f56fff5d40694ad1fe9c3706d3
yasche <172521093+yasche@users.noreply.github.com>
2025-10-04 18:45:30 +0200

    dna() prints a warning message if s contains non-nucleotides

--- src/dna.cpp
+++ src/dna.cpp
@@ -9,6 +9,8 @@ IntegerVector dna(std::string s)
   int n_g = { 0 };
   int n_t = { 0 };
 
+  std::string non_nucl = { "" };
+
   for(std::string::size_type i = 0; i < s.size(); ++i)
   {
     if (s[i] == 'A') {
@@ -19,8 +21,17 @@ IntegerVector dna(std::string s)
       n_g = n_g + 1;
     } else if (s[i] == 'C') {
       n_c = n_c + 1;
+    } else {
+      non_nucl += s[i];
     }
   }
+
+  if (non_nucl != "") {
+    std::string warning_message = {"The sequence `s` contains the following letters that do not correspond to a nucleotide: "};
+    warning_message += non_nucl;
+    warning(warning_message);
+  }
+
   IntegerVector return_vec = {n_a, n_c, n_g, n_t};
 
   return return_vec;
